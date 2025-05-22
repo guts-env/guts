823faf009389ed6f9ff0ad4891022ee5d8b0860e
cto-sherondavesanol <sheron@comfactechoptions.com>
2025-05-22 18:18:46 +0800

    Initial Commit

---
+++ lib/main.ts
@@ -0,0 +1,5 @@
+export function helloAnything(thing: string): string {
+  return `Hello ${thing}!`;
+}
+
+export * from "./components";
---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import path, { resolve } from "path";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss(), dts({ include: ["lib"] })],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./lib"),
+    },
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, "lib/main.ts"),
+      formats: ["es"],
+    },
+    copyPublicDir: false,
+    rollupOptions: {
+      external: ["react", "react/jsx-runtime"],
+    },
+  },
+});
